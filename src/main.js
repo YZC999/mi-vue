4beb9502565dd5bce4c939e81b5943ce5259cb70
buerjiaguniang <1183939108@qq.com>
2017-08-06 15:28:48 +0800

    jm_footer

--- src/main.js
+++ src/main.js
@@ -5,20 +5,14 @@ import App from './App'
 import router from './router'
 import  VueResource  from 'vue-resource'
 
-<<<<<<< HEAD
-Vue.use(VueResource) 
-=======
 import VueVideoPlayer from 'vue-video-player'
 
-
- import  VueResource  from 'vue-resource'
  Vue.use(VueResource) 
 
 
 // mount with global
 Vue.use(VueVideoPlayer)
 
->>>>>>> 89a59bea918999b74bbe0ec671a63d9f14d59afd
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
